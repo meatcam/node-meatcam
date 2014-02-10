b81dbb1a3587d586c1e16bb3b5574c2017957a33
imakewebthings <caleb@imakewebthings.com>
2014-02-09 18:11:10 -0800

    .

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 'use strict';
 
-var avconv = require('avconv');
-var gifsicle = require('gifsicle');
+//var avconv = require('avconv');
+//var gifsicle = require('gifsicle');
 var fs = require('fs');
+var exec = require('child_process').exec;
 
-var params = [
-  '-f', 'video4linux2',
-  '-i', '/dev/video0',
-  '-r', '10',
-  '-s', '320x240',
-  '-t', '2',
-  '-pix_fmt', 'rgb24',
-  '-vf', 'format=rgb8,format=rgb24'
-];
-
-avconv(params).pipe(fs.createWriteStream('test.gif'));
+exec('avconv -f video4linux2 -i /dev/video0 -r 10 -s 320x240 -t 2 -pix_fmt rgb24 -vf format=rgb8,format=rgb24 test.gif', function(err) {
+  console.log('finished');
+});
