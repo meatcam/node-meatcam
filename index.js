8b58941ffb9fa29217c40b51bf4804a4d10e462d
imakewebthings <caleb@imakewebthings.com>
2014-02-09 19:25:05 -0800

    .

--- index.js
+++ index.js
@@ -5,12 +5,16 @@ var exec = require('child_process').exec;
 
 function clean() {
   fs.unlink('out.gif', function(err) {
-    fs.unlink('outmin.gif', function(){});
+    console.log('out cleaned');
+    fs.unlink('outmin.gif', function(){
+      console.log('min cleaned');
+    });
   });
 }
 
 function base64(callback) {
   fs.readFile('outmin.gif', function(err, data) {
+    console.log('64ed');
     callback(data.toString());
     //clean();
   });
@@ -18,14 +22,18 @@ function base64(callback) {
 
 function compress(callback) {
   exec('gifsicle -O2 -o outmin.gif out.gif', function(err) {
+    console.log('compressed');
     base64(callback);
   });
 }
 
 function capture(callback) {
   exec('avconv -f video4linux2 -i /dev/video0 -r 10 -s 320x240 -t 2 -pix_fmt rgb24 -vf format=rgb8,format=rgb24 out.gif', function(err) {
+    console.log('captured');
     compress(callback);
   });
 }
 
-capture(function() {});
+capture(function() {
+  console.log('finished');
+});
