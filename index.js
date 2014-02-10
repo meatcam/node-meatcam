52ca767522890f9442ef3d89e91f1cd381a70faf
imakewebthings <caleb@imakewebthings.com>
2014-02-09 19:14:27 -0800

    .

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 'use strict';
 
-//var avconv = require('avconv');
-//var gifsicle = require('gifsicle');
 var fs = require('fs');
 var exec = require('child_process').exec;
 
-exec('avconv -f video4linux2 -i /dev/video0 -r 10 -s 320x240 -t 2 -pix_fmt rgb24 -vf format=rgb8,format=rgb24 test.gif', function(err) {
-  console.log('finished');
+function clean() {
+  fs.unlink('out.gif', function(err) {
+    fs.unlink('outmin.gif', function(){});
+  });
+}
+
+function base64(callback) {
+  fs.readFile('outmin.gif', function(err, data) {
+    callback(data.toString());
+  });
+}
+
+function compress(callback) {
+  exec('gifsicle -O2 -o outmin.gif out.gif', function(err) {
+    callback();
+  });
+}
+
+function capture(callback) {
+  exec('avconv -f video4linux2 -i /dev/video0 -r 10 -s 320x240 -t 2 -pix_fmt rgb24 -vf format=rgb8,format=rgb24 out.gif', function(err) {
+    callback();
+  });
+});
+
+capture(compress(base64(function() {
+  // DO THING
+  // clean();
 });
